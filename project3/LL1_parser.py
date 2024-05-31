05272cc3e9914da966d6c5b983157bdf035e1bef
wanke1997 <ke_wan_2019@hotmail.com>
2024-05-31 15:50:10 -0700

    bugfix

--- project3/LL1_parser.py
+++ project3/LL1_parser.py
@@ -145,11 +145,16 @@ class LL1Parser:
                     continue
                 for r in right:
                     # rule 2
-                    if len(r) >= 2 and r[-1] != "e" and r[-2] in self.non_terminals:
-                        prev_len = len(self.follow_dict[r[-2]])
-                        self.follow_dict[r[-2]] |= self.first_dict[r[-1]] - {"e"}
-                        if len(self.follow_dict[r[-2]]) > prev_len:
-                            set_updated = True
+                    if len(r) >= 2:
+                        for idx, ch in enumerate(r):
+                            if ch not in self.non_terminals or idx==len(r)-1:
+                                continue
+                            else:
+                                next_ch = r[idx+1]
+                                prev_len = len(self.follow_dict[ch])
+                                self.follow_dict[ch] |= (self.first_dict[next_ch]-{"e"})
+                                if len(self.follow_dict[ch]) > prev_len:
+                                    set_updated = True
                     # rule 3.1
                     if len(r) >= 1 and r[-1] in self.non_terminals:
                         prev_len = len(self.follow_dict[r[-1]])
@@ -157,11 +162,18 @@ class LL1Parser:
                         if len(self.follow_dict[r[-1]]) > prev_len:
                             set_updated = True
                     # rule 3.2
-                    if len(r) >= 2 and r[-2] in self.non_terminals and "e" in self.first_dict[r[-1]]:
-                        prev_len = len(self.follow_dict[r[-2]])
-                        self.follow_dict[r[-2]] |= self.follow_dict[x]
-                        if len(self.follow_dict[r[-2]]) > prev_len:
-                            set_updated = True
+                    if len(r) >= 2:
+                        for idx, ch in enumerate(r):
+                            if ch not in self.non_terminals or idx==len(r)-1:
+                                continue
+                            else:
+                                next_ch = r[idx+1]
+                                if "e" in self.first_dict[next_ch]:
+                                    prev_len = len(self.follow_dict[ch])
+                                    self.follow_dict[ch] |= self.follow_dict[x]
+                                    if len(self.follow_dict[ch]) > prev_len:
+                                        set_updated = True
+                        
 
     def build_chart(self) -> None:
         self.chart = {}
