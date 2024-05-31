2e8ae47024c69c9481c97f02d078abe1bc7a069e
wanke1997 <ke_wan_2019@hotmail.com>
2024-05-31 16:04:06 -0700

    implemented parse method, but still have bugs
    The bug comes from _build_states function. Sometimes the method cannot correctly find existing state's index because it only replies one item rather than all items.
    
    In order to fix this bug, We need to get the whole set, then use the set to find the index.

--- project5/SLR1_parser.py
+++ project5/SLR1_parser.py
@@ -24,17 +24,19 @@ class SLR1Parser:
             "T": ["T*F", "F"],
             "F": ["(E)", "i"],
         }
-        self.reverse_grammar: Dict[str, int] = {}
+        self.grammar_idx: Dict[str, int] = {}
+        self.reversed_grammar: Dict[str, str] = {}
         self.VT: Set[str] = set()
         self.VN: Set[str] = set()
         self.states: Dict[int, Set[str]] = {}
-        self.state_chart: Dict[Tuple[int, str], int] = {}
+        self.goto_chart: Dict[Tuple[int, str], int] = {}
+        self.action_chart: Dict[Tuple[int, str], str] = {}
         self.first_dict: Dict[str, Set[str]] = {}
         self.follow_dict: Dict[str, Set[str]] = {}
         self.preprocess()
         self.get_first_set()
         self.get_follow_set()
-        self.build_state_chart()
+        self.build_dfa()
 
     def read_file(self, filepath: Path) -> str:
         f = open(filepath, "r")
@@ -50,14 +52,19 @@ class SLR1Parser:
         return string
 
     def preprocess(self) -> None:
+        # build reversed grammar
+        for key, values in self.grammar.items():
+            for value in values:
+                self.reversed_grammar[value] = key
         # build VT and VN set
         # flatten the grammar as strings
         cnt = 0
+        self.VT.add("#")
         for key, values in self.grammar.items():
             self.VN.add(key)
             for value in values:
                 string = "{}->{}".format(key, value)
-                self.reverse_grammar[string] = cnt
+                self.grammar_idx[string] = cnt
                 cnt += 1
                 for ch in value:
                     if ch.isupper():
@@ -120,11 +127,16 @@ class SLR1Parser:
                     continue
                 for r in right:
                     # rule 2
-                    if len(r) >= 2 and r[-1] != "e" and r[-2] in self.VN:
-                        prev_len = len(self.follow_dict[r[-2]])
-                        self.follow_dict[r[-2]] |= self.first_dict[r[-1]] - {"e"}
-                        if len(self.follow_dict[r[-2]]) > prev_len:
-                            set_updated = True
+                    if len(r) >= 2:
+                        for idx, ch in enumerate(r):
+                            if ch not in self.VN or idx==len(r)-1:
+                                continue
+                            else:
+                                next_ch = r[idx+1]
+                                prev_len = len(self.follow_dict[ch])
+                                self.follow_dict[ch] |= (self.first_dict[next_ch]-{"e"})
+                                if len(self.follow_dict[ch]) > prev_len:
+                                    set_updated = True
                     # rule 3.1
                     if len(r) >= 1 and r[-1] in self.VN:
                         prev_len = len(self.follow_dict[r[-1]])
@@ -132,11 +144,43 @@ class SLR1Parser:
                         if len(self.follow_dict[r[-1]]) > prev_len:
                             set_updated = True
                     # rule 3.2
-                    if len(r) >= 2 and r[-2] in self.VN and "e" in self.first_dict[r[-1]]:
-                        prev_len = len(self.follow_dict[r[-2]])
-                        self.follow_dict[r[-2]] |= self.follow_dict[x]
-                        if len(self.follow_dict[r[-2]]) > prev_len:
-                            set_updated = True
+                    if len(r) >= 2:
+                        for idx, ch in enumerate(r):
+                            if ch not in self.VN or idx==len(r)-1:
+                                continue
+                            else:
+                                next_ch = r[idx+1]
+                                if "e" in self.first_dict[next_ch]:
+                                    prev_len = len(self.follow_dict[ch])
+                                    self.follow_dict[ch] |= self.follow_dict[x]
+                                    if len(self.follow_dict[ch]) > prev_len:
+                                        set_updated = True
+    def reduce_sentence(self, input: str) -> str:
+        # hard coded, if use another grammar, then we need to rewrite this method
+        if input is None or len(input) == 0:
+            raise Exception("error: the input string is empty.")
+        elif len(input) == 1:
+            if input in {"i", "F", "T"}:
+                return self.reversed_grammar[input]
+            elif input == "E":
+                return "E"
+            else:
+                raise Exception("error: unexpected character, {}".format(input))
+        elif len(input) == 3:
+            if input[1] == "+" and input[0] in self.VN and input[2] in self.VN:
+                return self.reversed_grammar["E+T"]
+            elif input[1] == "-" and input[0] in self.VN and input[2] in self.VN:
+                return self.reversed_grammar["E-T"]
+            elif input[1] == "*" and input[0] in self.VN and input[2] in self.VN:
+                return self.reversed_grammar["T*F"]
+            elif input[1] == "/" and input[0] in self.VN and input[2] in self.VN:
+                return self.reversed_grammar["T/F"]
+            elif input[0] == "(" and input[2] == ")" and input[1] in self.VN:
+                return self.reversed_grammar["(E)"]
+            else:
+                raise Exception("error: unexpected character, {}".format(input))
+        else:
+            raise Exception("error: unexpected character, {}".format(input))
 
     def _build_initial_items(self, left: str) -> Set[str]:
         """
@@ -201,7 +245,7 @@ class SLR1Parser:
                 res |= self._build_closure(next_item)
             return res
 
-    def _goto(self, state_idx: int, input: str) -> int:
+    def _go(self, state_idx: int, input: str) -> int:
         state: Set[str] = self.states[state_idx]
         total = len(self.states)
         items: Set[str] = set()
@@ -212,7 +256,11 @@ class SLR1Parser:
                 next_item_closure = self._build_closure(next_item)
                 items |= next_item_closure
         self.states[total] = items
-        self.state_chart[(state_idx, input)] = total
+        if input in self.VN:
+            print("####", str((state_idx, input)), str(total))
+            self.goto_chart[(state_idx, input)] = total
+        else:
+            self.action_chart[(state_idx, input)] = "S{}".format(total)
         return total
 
     def _find_item_state(self, item: str) -> Optional[int]:
@@ -220,23 +268,53 @@ class SLR1Parser:
             if item in items:
                 return idx
         return None
+    
+    def _find_terminal_item(self, idx: int) -> Optional[str]:
+        for item in self.states[idx]:
+            if item[-1] == ".":
+                return item
+        return None
+
+    def _find_grammar(self, idx: int) -> str:
+        for key, value in self.grammar_idx.items():
+            if value == idx:
+                return key
+        return ""
+    
+    # def _find_set_index(self, dict: Dict[str, Set[str]], next_chr: str) -> Optional[int]:
+    #     set = dict[next_chr]
+
 
     def _build_states(self, state_idx: int) -> None:
         state: Set[str] = self.states[state_idx]
+        # dict: Dict[str, Set[str]] = {}
+        # for item in state:
+        #     next_chr = item[item.index(".") + 1]
+        #     next_item = self._get_next_item(item)[0]
+        #     if next_chr in dict:
+        #         dict[next_chr].add(next_item)
+        #     else:
+        #         dict[next_chr] = {next_item}
+
         for item in state:
             if item[-1] == ".":
                 continue
             else:
+                next_chr = item[item.index(".") + 1]
                 potencial_next_idx = self._find_item_state(self._get_next_item(item)[0])
                 if potencial_next_idx is not None:
-                    self.state_chart[(state_idx, item[item.index(".") + 1])] = potencial_next_idx
-                    # add the result to self.state_chart
+                    
+                    if next_chr in self.VN:
+                        print("####", str((state_idx, input)), str(potencial_next_idx))
+                        self.goto_chart[(state_idx, next_chr)] = potencial_next_idx
+                    else:
+                        self.action_chart[(state_idx, next_chr)] = "S{}".format(potencial_next_idx)
                 else:
                     dot_idx = item.index(".")
-                    next_idx = self._goto(state_idx, item[dot_idx + 1])
+                    next_idx = self._go(state_idx, item[dot_idx + 1])
                     self._build_states(next_idx)
 
-    def build_state_chart(self) -> None:
+    def build_dfa(self) -> None:
         # initialize start point
         start_item: Set[str] = self._build_initial_items(self.start)
         if start_item is None or len(start_item) != 1:
@@ -245,6 +323,53 @@ class SLR1Parser:
         self.states[0] = start_closure
         # start loop to find all states
         self._build_states(state_idx=0)
+        for idx in range(len(self.states)):
+            T_item = self._find_terminal_item(idx)
+            if T_item is None:
+                continue
+            left = T_item.split("->")[0]
+            for a in self.follow_dict[left]:
+                r_idx = self.grammar_idx[T_item[:-1]]
+                self.action_chart[(idx, a)] = "r{}".format(r_idx)
+            if left == self.start:
+                self.action_chart[(idx, "#")] = "acc"
+
+    def parse(self, filepath: Path) -> bool:
+        string = self.read_file(filepath) + "#"
+        pt = 0
+        state_stack = [0]
+        sign_stack = "#"
+        while pt < len(string):
+            print("status stack: {}".format(state_stack))
+            print("sign stack: {}".format(sign_stack))
+            print("input string: {}".format(string[pt:]))
+            cur_state = state_stack[-1]
+            cur_input = string[pt]
+            action = self.action_chart[(cur_state, cur_input)]
+            if action.startswith("S"):
+                next_state = int(action[1:])
+                state_stack.append(next_state)
+                sign_stack += cur_input
+                pt += 1
+            elif action.startswith("r"):
+                # 1. reduce
+                idx = int(action[1:])
+                terminal_item = self._find_grammar(idx)
+                length = len(terminal_item.split("->")[1])
+                sign_stack = sign_stack[:len(sign_stack)-length]
+                sign_stack += terminal_item.split("->")[0]
+                # 2. pop the current state
+                state_stack = state_stack[:len(state_stack)-length]
+                # 3. call goto to get next state
+                next_state = self.goto_chart[(state_stack[-1], sign_stack[-1])]
+                # 4. update the next state
+                state_stack.append(next_state)
+            elif action == "acc":
+                return True
+            else:
+                raise Exception("error: action {} error".format(action))
+            print("#"*30)
+        return False
 
 
 if __name__ == "__main__":
@@ -252,8 +377,11 @@ if __name__ == "__main__":
     testcase = "test_case1.txt"
     filepath = Path(CURRENT_FILE_PATH).joinpath(DEFAULT_TESTCASE_DIR).joinpath(testcase)
     string = parser.read_file(filepath)
-    # print("The input string is: {}".format(string))
-    print(len(parser.states))
-    print(len(parser.state_chart))
-    for (state, input), value in parser.state_chart.items():
-        print("{}, {} : {}".format(state, input, value))
+    # for key, value in parser.action_chart.items():
+    #     print(key, value)
+    # for key, value in parser.goto_chart.items():
+    #     print(key, value)
+    for key, value in parser.states.items():
+        print(key, value)
+    res = parser.parse(filepath)
+    print(res)
